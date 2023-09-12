5c69a149c15a0aa87b2fb219c3f7222c21aad06f
howudong <tjdvy963@naver.com>
2023-09-12 14:17:29 +0900

    feat : Set Collection 테스트 요구 사항

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,9 +1,16 @@
 package study;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -15,5 +22,25 @@ public class SetTest {
         numbers.add(1);
         numbers.add(2);
         numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set의 사이즈가 3이면 성공한다.")
+    public void set_사이즈_정상_테스트() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("number을 찾을때 가지고 있다면 성공한다.")
+    public void set_원소_포함_테스트(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1,true", "2,true", "3,true", "4,false", "5,false"})
+    @DisplayName("number을 찾을때 가지고 있다면 성공한다.")
+    public void set_원소_포함_테스트(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
     }
 }
